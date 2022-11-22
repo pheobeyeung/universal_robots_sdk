a9a706d3e8c63cebc30330e792d82253216ef0b8
Pheobe Yeung <46576808+pheobeyeung@users.noreply.github.com>
2022-11-22 12:28:53 -0500

    Update startURSim.sh

--- startURSim.sh
+++ startURSim.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-
-cd /
-./entrypoint.sh &
-
-cd /ursim 
-./start-ursim.sh &
-
+{
 xdg-open http://localhost:6080/vnc.html?host=localhost&port=6080
+}&
+{
+cd /ursim 
+./start-ursim.sh 
+}&
+{
+    cd /
+    ./entrypoint.sh
+}
+
+
+
+

c87cf2057faee27cdea95b4d19c692b94083bec2
palmmaximilian <35168544+palmmaximilian@users.noreply.github.com>
2026-02-13 22:45:09 +0100

    added ursim webapp

---
+++ sdk-1.18.0/samples/swing/com.ur.urcap.examples.mydaemonswing/target/classes/com/ur/urcap/examples/mydaemonswing/impl/daemon/hello-world.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SocketServer import ThreadingMixIn
+
+title = ""
+
+def isReachable():
+	return True
+
+def set_title(new_title):
+	global title
+	title = new_title
+	return title
+
+def get_title():
+	tmp = ""
+	if str(title):
+		tmp = title
+	else:
+		tmp = "No title set"
+	return tmp + " (Python)"
+
+def get_message(name):
+	if str(name):
+		return "Hello " + str(name) + ", welcome to PolyScope!"
+	else:
+		return "No name set"
+
+sys.stdout.write("MyDaemon daemon started")
+sys.stderr.write("MyDaemon daemon started")
+
+class MultithreadedSimpleXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
+	pass
+
+server = MultithreadedSimpleXMLRPCServer(("127.0.0.1", 40405))
+server.register_function(isReachable, "isReachable")
+server.RequestHandlerClass.protocol_version = "HTTP/1.1"
+server.register_function(set_title, "set_title")
+server.register_function(get_title, "get_title")
+server.register_function(get_message, "get_message")
+server.serve_forever()
